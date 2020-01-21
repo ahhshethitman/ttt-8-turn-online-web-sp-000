5aa37ef5621ce497fab1ea5c57a645977958b045
Hunter Sides <hunter.s.1337@gmail.com>
2020-01-21 16:16:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,15 @@ def valid_move?(board, index)
 end
 
 
-def turn(board)
+def turn (board)
   puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
